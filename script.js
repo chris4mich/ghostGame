ce97afbfca68be814ff890858344d3e261ff3716
chrismich <chrs.mich@gmail.com>
2023-09-05 13:40:23 +0300

    more updates

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const scoreElement = document.getElementById("score");
   const oou = document.getElementById("oou");
   const scream = document.getElementById("scream");
+  const scaryFace = document.getElementById("scary-face");
 
+  let screamDelay = generateScreamDelay();
   let lastGhostPosition = { x: 0, y: 0 };
   let score = 0;
 
   const rangeX = stage.clientWidth - ghost.clientWidth;
   const rangeY = stage.clientHeight - ghost.clientHeight;
-  
+
+  function generateScreamDelay() {
+    return Math.floor(Math.random() * 15) + 6;
+  }
 
   function moveghostRandomly() {
     const maxX = rangeX;
@@ -24,26 +29,32 @@ document.addEventListener("DOMContentLoaded", function () {
     lastGhostPosition = { x: randomX, y: randomY };
   }
 
+  function handleScreamEvent() {
+    scream.play();
+    scaryFace.style.display = "block";
+    setTimeout(() => {
+      scaryFace.style.display = "none";
+    }, 800);
+    score = 0;
+    screamDelay = generateScreamDelay();
+  }
+
   function disappearOnClick() {
     ghost.style.display = "none";
     skull.style.display = "block";
 
     skull.style.transform = `translate(${lastGhostPosition.x}px, ${lastGhostPosition.y}px)`;
 
-    const screamDelay = Math.floor(Math.random() * 20);
-
     if (score < 50) {
       score++;
       scoreElement.textContent = `Score: ${score}`;
     }
-    console.log(screamDelay)
-
-    if (score === screamDelay && score > 5){
-      scream.play();
-      console.log("now !!!")
-    };
-
-    oou.play();
+    console.log(screamDelay);
+    if (score === screamDelay && score > 5) {
+      handleScreamEvent();
+    } else {
+      oou.play();
+    }
 
     setTimeout(() => {
       ghost.style.display = "block";
